3a268997e1ba67d40760bcc7ff458efef99d7e57
DUCNA1 <nguyenanhduchatake2003@gmail.com>
2024-11-01 20:43:05 +0700

    jsx and css inline #30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,5 +1,14 @@
+import "./style.css";
+
 const MyComponent = () => {
-  return <div>Xin chao Nguyen Anh Duc</div>;
+  return (
+    <>
+      <div className="name" style={{ fontSize: "30px" }}>
+        Xin chao Nguyen Anh Duc
+      </div>
+      <button>Click me</button>
+    </>
+  );
 };
 
 export default MyComponent;
