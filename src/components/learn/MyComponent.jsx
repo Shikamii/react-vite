dae02ab2adad7f699da04c54dbc25835eaa48cac
DUCNA1 <nguyenanhduchatake2003@gmail.com>
2024-11-01 21:01:47 +0700

    cách sử dụng biến số với jsx #31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,11 +1,19 @@
 import "./style.css";
 
 const MyComponent = () => {
+  const name = "Nguyen Anh Duc";
+  const company = "HBlab";
+  const object = {
+    name: "Ducna1",
+    age: 21,
+  };
   return (
     <>
       <div className="name" style={{ fontSize: "30px" }}>
-        Xin chao Nguyen Anh Duc
+        Xin chao {name}
       </div>
+      <div>xin chao {company}</div>
+      <div>xin chao {JSON.stringify(object)}</div>
       <button>Click me</button>
     </>
   );
