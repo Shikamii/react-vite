b2047e03d8314ef670a2390d952e68bc94c0ccde
DUCNA1 <nguyenanhduchatake2003@gmail.com>
2024-11-03 00:15:03 +0700

    su kien onClick and onChange #36

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,9 +1,20 @@
 const TodoNew = (props) => {
   console.log({ props });
   const { addNewTodo } = props;
+
+  // const handleOnChange = (event) => {
+  //   console.log(event.target.value);
+  // };
+  const handleOnChange = (name) => {
+    console.log(name);
+  };
   return (
     <div className="todo_new">
-      <input type="text" placeholder="Enter your task" />
+      <input
+        type="text"
+        placeholder="Enter your task"
+        onChange={(event) => handleOnChange(event.target.value)}
+      />
       <button onClick={addNewTodo}>Add</button>
     </div>
   );
