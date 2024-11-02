6cf956ec9a8af8734f961a9480989fc0d83d428e
DUCNA1 <nguyenanhduchatake2003@gmail.com>
2024-11-02 23:34:07 +0700

    props #34

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,18 @@ import reactLogo from "./assets/react.svg";
 import TodoNew from "./components/todo/TodoNew";
 import TodoData from "./components/todo/TodoData";
 const App = () => {
+  const name = "Nguyen Anh Duc";
+  const age = 25;
+  const data = {
+    company: "HBlab",
+    address: "219 trung kinh ",
+  };
   return (
     <>
       <div className="todo_container">
         <div className="todo_title">Todo List</div>
         <TodoNew />
-        <TodoData />
+        <TodoData name={name} age={age} data={data} />
         <div className="todo_image">
           <img src={reactLogo} alt="" className="logo react" />
         </div>
--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -1,6 +1,12 @@
-const TodoData = () => {
+const TodoData = (props) => {
+  console.log({ props });
+
+  const { name, age, data } = props;
   return (
     <div className="todo_data">
+      <div>My name is {name}</div>
+      <div>My name is {age}</div>
+      <div>My name is {JSON.stringify(data)}</div>
       <div>Learning React</div>
     </div>
   );
